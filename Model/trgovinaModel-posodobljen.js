272b355c0d392b205558b1d81cff03eacc6eb147
GM-Coder007 <91761395+GM-Coder007@users.noreply.github.com>
2022-04-27 16:46:48 +0200

    Posodobljen model zbirke Trgovina

---
+++ Model/trgovinaModel-posodobljen.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema   = mongoose.Schema;
+
+var trgovinaSchema = new Schema({
+	'Naziv' : String,
+	'Celoten_naziv' : String,
+	'Naslov': String,
+	'Posta' : Number,
+	'Kraj' : String,
+	'Lastnik': String,
+	'Spletna_stran' : String	
+});
+
+
+
+trgovinaSchema.pre('save', function(next){
+	var trgovina = this;
+	next();
+});
+
+var Trgovina = mongoose.model('Trgovina', trgovinaSchema);
+module.exports = Trgovina;
