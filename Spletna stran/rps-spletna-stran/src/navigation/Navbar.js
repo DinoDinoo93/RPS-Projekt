dddb2657298cc59200396c379feff2fd6e4fb033
DinoDinoo93 <57443334+DinoDinoo93@users.noreply.github.com>
2022-05-06 00:18:06 +0200

    Komentar
    Komentar

--- Spletna stran/rps-spletna-stran/src/navigation/Navbar.js
+++ Spletna stran/rps-spletna-stran/src/navigation/Navbar.js
@@ -9,9 +9,11 @@ const Navbar = () => {
                     <li class="nav-item active">
                         <a class="nav-link" href="/">Domov<span class="sr-only">(current)</span></a>
                     </li>
+                    {/*
                     <li class="nav-item">
                         <a class="nav-link" href="/listItems">Izdelki</a>
                     </li>
+                    */}
                 </ul>
             </div>
         </nav>
