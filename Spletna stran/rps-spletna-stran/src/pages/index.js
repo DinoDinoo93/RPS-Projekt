8964753d441d83b9271d03ba3bb71eeb88d3a789
DinoDinoo93 <57443334+DinoDinoo93@users.noreply.github.com>
2022-05-05 23:27:34 +0200

    Prototip
    Mnda je narjen

--- Spletna stran/rps-spletna-stran/src/pages/index.js
+++ Spletna stran/rps-spletna-stran/src/pages/index.js
@@ -1,16 +1,52 @@
 
-import React from "react";
+import React, { useState } from 'react';
 import Carousel from 'react-grid-carousel'
 import data from "../data.json"
+import { useNavigate } from "react-router-dom";
+
+
+
 const Home = () => {
-  return (
+    let textInput = React.createRef();
+    let navigate = useNavigate();
+    /*
+    function searchEvent(e) {
+        data.filter((val)=>{
+            if(searchTerm == ""){
+              return val
+            }
+            else if(val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+              return val;
+            }
+          }).map((val,key)=>{
+            return <div>{val.Title} </div>
+          })
+    }
+    */
+    let onOnclickHandler = (e) => {
+        data.resources.filter((val)=>{
+            if(val.title.toLowerCase().includes(textInput.current.value.toLowerCase())){
+              return val;
+            }
+            else {
+              console.log("Ni najdel");
+              console.log(val.title.toLowerCase());
+              console.log(textInput.current.value.toLowerCase())
+            }
+          }).map((val,key)=>{
+            console.log("najdel!!!!!");
+            navigate("/item?index=" + val.index);
+          })
+    };
+
+    return (
     <div class="flex h-screen">
         <div class="m-auto">
             <h1>Poišči izdelek</h1>
             <div class="mb-3 xl:w-96">
                 <div class="input-group relative flex flex-wrap items-stretch w-full mb-4">
-                    <input type="search" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon3" />
-                    <button class="btn inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3">Search</button>
+                    <input ref={textInput} type="text" class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" placeholder="Search" aria-label="Search" aria-describedby="button-addon3" />
+                    <button onClick={onOnclickHandler} class="btn inline-block px-6 py-2 border-2 border-blue-600 text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" type="button" id="button-addon3">Search</button>
                 </div>
             </div>
             <div class="max-w-lg">
