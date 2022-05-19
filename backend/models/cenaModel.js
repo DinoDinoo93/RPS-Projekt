9437be465b88447124edc96a5a2ddf8a611536ff
GM-Coder007 <91761395+GM-Coder007@users.noreply.github.com>
2022-05-19 18:11:44 +0200

    Posodobitev modela Cena (dodan id_izdelek)

--- backend/models/cenaModel.js
+++ backend/models/cenaModel.js
@@ -4,7 +4,8 @@ var Schema   = mongoose.Schema;
 var cenaSchema = new Schema({
 	'id_izdelka' : String,
 	'Datum_cas' : Date,
-	'Cena' : Number
+	'Cena' : Number,
+	'id_izdelek' : String // id izdelka iz spletne strani
 });
 
 cenaSchema.pre('save', function(next){
