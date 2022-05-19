e5d85f631f19244ac352360c5801de584659a58e
GM-Coder007 <91761395+GM-Coder007@users.noreply.github.com>
2022-05-19 18:14:24 +0200

    Posodobljen model Izdelek (dodan id_izdelek)

--- backend/models/izdelekModel.js
+++ backend/models/izdelekModel.js
@@ -6,8 +6,10 @@ var izdelekSchema = new Schema({
 	'Naziv' : String,
 	'Vrsta' : String,
 	'Opis' : String,
+	'Podjetje' : String,
 	'Slika' : String,
-	'Kategorija' : String
+	'Kategorija' : String,
+	'id_izdelek' : String // za pobiranje ID-ja iz spletne strani (na spletni strani ima izdelek svoj ID)
 });
 
 izdelekSchema.pre('save', function(next){
